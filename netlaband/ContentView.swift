3fb864fec45c213d34034d1791079d710d629c34
Joe Heck <heckj@mac.com>
2019-12-27 10:32:59 -0800

    Initial Commit

---
+++ netlaband/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  netlaband
+//
+//  Created by Joseph Heck on 12/27/19.
+//  Copyright © 2019 JFH Consulting. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ netlabandTests/netlabandTests.swift
@@ -0,0 +1,34 @@
+//
+//  netlabandTests.swift
+//  netlabandTests
+//
+//  Created by Joseph Heck on 12/27/19.
+//  Copyright © 2019 JFH Consulting. All rights reserved.
+//
+
+import XCTest
+@testable import netlaband
+
+class netlabandTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
