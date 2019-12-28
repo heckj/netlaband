5e7920c971afca4be2dc7e4b6be9e3a7df51e073
Joe Heck <heckj@mac.com>
2019-12-28 15:55:03 -0800

    test baseline

--- netlabandTests/netlabandTests.swift
+++ netlabandTests/netlabandTests.swift
@@ -23,10 +23,10 @@ class NetlabandTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 }
