e80453dd802451d70ee2c84caeb59c4f726b990b
Joe Heck <heckj@mac.com>
2020-03-08 16:59:16 -0700

    killing the insets and normalizing the variables of the external tick views

--- netlaband/HorizontalAxisView.swift
+++ netlaband/HorizontalAxisView.swift
@@ -10,44 +10,32 @@ import SwiftUI
 import SwiftViz
 
 public struct HorizontalAxisView<ScaleType: Scale>: View {
-    let leftInset: CGFloat
-    let rightInset: CGFloat
     var scale: ScaleType
-    init(scale: ScaleType, leftInset: CGFloat = 10, rightInset: CGFloat = 10) {
-        self.leftInset = leftInset
-        self.rightInset = rightInset
+    let inset: CGFloat = 3.0
+    let tickLength: CGFloat = 5.0
+    var numTicks = 10
+
+    init(scale: ScaleType) {
         self.scale = scale
     }
 
     func tickList(geometry: GeometryProxy) -> [ScaleType.TickType] {
-        // protect against Preview sending in stupid values
-        // of geometry that can't be made into a reasonable range
-        // otherwise the next line will crash preview...
-        if geometry.size.width < leftInset + rightInset {
-            return [ScaleType.TickType]()
-        }
-        let geometryRange = 0.0 ... CGFloat(geometry.size.width - leftInset - rightInset)
-        return scale.ticks(count: 10, range: geometryRange)
+        let geometryRange = 0.0 ... CGFloat(geometry.size.width)
+        return scale.ticks(count: numTicks, range: geometryRange)
     }
 
     public var body: some View {
         GeometryReader { geometry in
             ZStack {
                 Path { path in
-                    if geometry.size.width < self.leftInset + self.rightInset {
-                        // preview can call this with pathological values that may not make sense...
-                        // in case the reported width is tiny (less than insets), bail!
-                        return
-                    }
-
                     // draw base axis line
-                    path.move(to: CGPoint(x: self.leftInset, y: 3))
-                    path.addLine(to: CGPoint(x: geometry.size.width - self.rightInset, y: 3))
+                    path.move(to: CGPoint(x: 0, y: self.inset))
+                    path.addLine(to: CGPoint(x: geometry.size.width, y: self.inset))
 
                     // draw each tick in the line
                     for tick in self.tickList(geometry: geometry) {
-                        path.move(to: CGPoint(x: tick.rangeLocation + self.leftInset, y: 3))
-                        path.addLine(to: CGPoint(x: tick.rangeLocation + self.leftInset, y: 8))
+                        path.move(to: CGPoint(x: tick.rangeLocation, y: self.inset))
+                        path.addLine(to: CGPoint(x: tick.rangeLocation, y: self.inset + self.tickLength))
                     }
                 }.stroke()
             }
@@ -62,15 +50,13 @@ public struct HorizontalAxisView<ScaleType: Scale>: View {
 struct HorizontalAxisView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            HorizontalAxisView(scale: LinearScale(domain: 0 ... 5.0, isClamped: false),
-                               leftInset: 25.0,
-                               rightInset: 25.0)
+            HorizontalAxisView(scale: LinearScale(domain: 0 ... 5.0, isClamped: false))
                 .frame(width: 400, height: 50, alignment: .center)
+                .padding()
 
-            HorizontalAxisView(scale: LogScale(domain: 1 ... 10.0, isClamped: false),
-                               leftInset: 25.0,
-                               rightInset: 25.0)
+            HorizontalAxisView(scale: LogScale(domain: 1 ... 10.0, isClamped: false))
                 .frame(width: 400, height: 50, alignment: .center)
+                .padding()
         }
     }
 }
--- netlaband/VerticalAxisView.swift
+++ netlaband/VerticalAxisView.swift
@@ -10,46 +10,31 @@ import SwiftUI
 import SwiftViz
 
 struct VerticalAxisView<ScaleType: Scale>: View {
-    let topInset: CGFloat
-    let bottomInset: CGFloat
-    let leftOffset: CGFloat
-    let tickLength: CGFloat
+    let inset: CGFloat = 3.0
+    let tickLength: CGFloat = 5.0
+    var numTicks = 10
 
     var scale: ScaleType
-    init(scale: ScaleType, topInset: CGFloat = 25, bottomInset: CGFloat = 25) {
-        self.topInset = topInset
-        self.bottomInset = bottomInset
+    init(scale: ScaleType) {
         self.scale = scale
-        leftOffset = 30
-        tickLength = 5
     }
 
     func tickList(geometry: GeometryProxy) -> [ScaleType.TickType] {
-        // protect against Preview sending in stupid values
-        // of geometry that can't be made into a reasonable range
-        // otherwise the next line will crash preview...
-        if geometry.size.width < topInset + bottomInset {
-            return [ScaleType.TickType]()
-        }
-        let geometryRange = 0.0 ... CGFloat(geometry.size.height - topInset - bottomInset)
-        return scale.ticks(count: 10, range: geometryRange)
+        let geometryRange = 0.0 ... CGFloat(geometry.size.height)
+        return scale.ticks(count: numTicks, range: geometryRange)
     }
 
     var body: some View {
         GeometryReader { geometry in
             ZStack {
                 Path { path in
-                    // guard against too small:
-                    if geometry.size.height < self.topInset + self.bottomInset {
-                        return
-                    }
 
-                    path.move(to: CGPoint(x: self.leftOffset + self.tickLength, y: self.topInset))
-                    path.addLine(to: CGPoint(x: self.leftOffset + self.tickLength, y: geometry.size.height - self.bottomInset))
+                    path.move(to: CGPoint(x: self.inset + self.tickLength, y: 0))
+                    path.addLine(to: CGPoint(x: self.inset + self.tickLength, y: geometry.size.height))
 
                     for tick in self.tickList(geometry: geometry) {
-                        path.move(to: CGPoint(x: self.leftOffset, y: tick.rangeLocation + self.topInset))
-                        path.addLine(to: CGPoint(x: self.leftOffset + self.tickLength, y: tick.rangeLocation + self.topInset))
+                        path.move(to: CGPoint(x: self.inset, y: tick.rangeLocation))
+                        path.addLine(to: CGPoint(x: self.inset + self.tickLength, y: tick.rangeLocation))
                     }
                 }.stroke()
 //                ForEach(self.tickList(geometry: geometry)) { tickStruct in
@@ -65,9 +50,11 @@ struct VerticalAxisView_Previews: PreviewProvider {
         Group {
             VerticalAxisView(scale: LinearScale(domain: 0 ... 1.0, isClamped: false))
                 .frame(width: 100, height: 400, alignment: .center)
+                .padding()
 
             VerticalAxisView(scale: LogScale(domain: 1 ... 10.0, isClamped: false))
                 .frame(width: 100, height: 400, alignment: .center)
+                .padding()
         }
     }
 }
