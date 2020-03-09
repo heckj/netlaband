bf5ccf6a2770549d43e9ca5dce90acd475451b7d
Joe Heck <heckj@mac.com>
2020-03-09 11:54:01 -0700

    tick labels

--- netlaband/HorizontalTickDisplayView.swift
+++ netlaband/HorizontalTickDisplayView.swift
@@ -9,9 +9,57 @@
 import SwiftUI
 import SwiftViz
 
-struct HorizontalTickDisplayView: View {
+struct HorizontalTickDisplayView<ScaleType: Scale>: View {
+    let scale: ScaleType
+    let numTicks = 10
+    let numberFormatter = NumberFormatter()
+
+    init(scale: ScaleType) {
+        self.scale = scale
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.minimumFractionDigits = 0
+        numberFormatter.maximumFractionDigits = 1
+    }
+
+    func tickList(geometry: GeometryProxy) -> [ScaleType.TickType] {
+        // protect against Preview sending in stupid values
+        // of geometry that can't be made into a reasonable range
+        // otherwise the next line will crash preview...
+        let geometryRange = 0.0 ... CGFloat(geometry.size.width)
+        return scale.ticks(count: numTicks, range: geometryRange)
+    }
+
+    func tickLabel(_ tick: ScaleType.TickType) -> String {
+        // let foo = String(format: "%.1f", tick.value)
+        // this runs into all sorts of freakish issues converting
+        // the inferred type into CVarArg for the
+        // implied NumberFormatter under the covers
+
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        numberFormatter.minimumFractionDigits = 0
+        numberFormatter.maximumFractionDigits = 1
+
+        // to avoid this force-cast nonsense, which is otherwise
+        // required for the compilation, we might want to
+        // twiddle Tick (the protocol) to explictly return
+        // an NSNumber
+        let formattedString = numberFormatter.string(from: tick.value as! NSNumber) ?? "erp"
+
+        return formattedString
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ZStack {
+                ForEach(self.tickList(geometry: geometry)) { tick in
+                    Text(self.tickLabel(tick))
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color.primary)
+                        .position(x: tick.rangeLocation, y: 0)
+                }
+            }
+        }
     }
 }
 
@@ -19,23 +67,50 @@ struct HorizontalTickDisplayView: View {
     struct HorizontalTickDisplayView_Previews: PreviewProvider {
         static var previews: some View {
             Group {
+                // axis view w/ linear scale - simple/short
+                VStack {
+                    HorizontalAxisView(scale: LinearScale(domain: 0 ... 5.0, isClamped: false))
+                    HorizontalTickDisplayView(scale: LinearScale(domain: 0 ... 5.0, isClamped: false))
+                }
+                .frame(width: 400, height: 50, alignment: .center)
+                .padding()
+
+                // band view w/ linear scale - simple/short
                 VStack {
                     HorizontalBandView(scale: LinearScale(domain: 0 ... 5.0, isClamped: false))
-                    HorizontalTickDisplayView()
+                    HorizontalTickDisplayView(scale: LinearScale(domain: 0 ... 5.0, isClamped: false))
                 }
                 .frame(width: 400, height: 50, alignment: .center)
                 .padding()
 
+                // axis view w/ log scale variant - simple/short
+                VStack {
+                    HorizontalAxisView(scale: LogScale(domain: 1 ... 10.0, isClamped: false))
+                    HorizontalTickDisplayView(scale: LogScale(domain: 1 ... 10.0, isClamped: false))
+                }
+                .frame(width: 400, height: 50, alignment: .center)
+                .padding()
+
+                // band view w/ log scale variant - simple/short
                 VStack {
                     HorizontalBandView(scale: LogScale(domain: 1 ... 10.0, isClamped: false))
-                    HorizontalTickDisplayView()
+                    HorizontalTickDisplayView(scale: LogScale(domain: 1 ... 10.0, isClamped: false))
                 }
                 .frame(width: 400, height: 50, alignment: .center)
                 .padding()
 
+                // axis view w/ log scale variant - longer
+                //
+                // dense logScales look pretty rough here
+                // so maybe one thing to do would be to make an
+                // indicator on each tick if it's "major" or "minor"
+                // which could be used within the band/scale to
+                // improve the visualization (bolder/darker line)
+                // and also to provide a labeling hint
+                // "show or not-show"
                 VStack {
                     HorizontalBandView(scale: LogScale(domain: 0.1 ... 100.0, isClamped: false))
-                    HorizontalTickDisplayView()
+                    HorizontalTickDisplayView(scale: LogScale(domain: 0.1 ... 100.0, isClamped: false))
                 }
                 .frame(width: 400, height: 50, alignment: .center)
                 .padding()
