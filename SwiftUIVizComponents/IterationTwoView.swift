f14be0713f60c3574cfbed66b7f3c0cf17df169b
Joe Heck <heckj@mac.com>
2020-03-19 17:26:20 -0700

    better vertical time view

--- SwiftUIVizComponents/IterationTwoView.swift
+++ SwiftUIVizComponents/IterationTwoView.swift
@@ -14,15 +14,18 @@ struct IterationTwoView: View {
     @ObservedObject var networkModel: NetworkAnalyzer
     @State private var metrics = CircularBuffer<NetworkAnalysisDataPoint>(initialCapacity: 50)
 
+    func calculatedDistanceNeeded() -> CGFloat {
+        CGFloat(Double(networkModel.timerinterval) * Double(capacity) / Double(networkModel.urlsToValidate.count))
+    }
+
     var body: some View {
         VStack {
             HStack {
                 Text("\(metrics.count) datapoints")
             }
             DataPointCollectionView2(points: self.metrics,
-                                     scale: LogScale(domain: 1 ... 10000.0, isClamped: false))
-//                .frame(height: 250, alignment: .center)
-//                .padding()
+                                     scale: LogScale(domain: 1 ... 10000.0, isClamped: true), maxDurationNeeded: calculatedDistanceNeeded())
+                .padding()
         }.onReceive(networkModel.metricPublisher.receive(on: RunLoop.main), perform: { dp in
             self.metrics.append(dp)
             if self.metrics.count > self.capacity {
